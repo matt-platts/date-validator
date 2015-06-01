9415db98177ec99625f91acae7f9a1e1959b6538
Matt Platts <mattplatts@gmail.com>
2015-06-01 20:21:28 +0100

    Add regex check too

--- date-validator.js
+++ date-validator.js
@@ -1,16 +1,20 @@
 // Date validator
 // by Matt Platts (mattplatts@gmail.com)
-// validates that a date is valid and not the 31st of february for example....
+// Validates that a date is valid and not the 31st of february for example....
 
 function validateDate(dateText){
 
-    var comp = dateText.split(/[\/-]/);
+    // first a regex check for dd/mm/yyyy or dd-mm-yyyy (both are supported)
+    var re = /\d{2}(\/|-)\d{2}(\/|-)\d{4}/;
+    if (!dateText.match(re)){ return false; }
+	
+    var components = dateText.split(/[\/-]/);
 
-    var d = parseInt(comp[0], 10);
-    var m = parseInt(comp[1], 10);
-    var y = parseInt(comp[2], 10);
+    var d = parseInt(components[0], 10);
+    var m = parseInt(components[1], 10);
+    var y = parseInt(components[2], 10);
     var date = new Date(y+"/"+m+"/"+d);
-    m--;
+    m--; // because months are indexed from 0 not 1
 
   return date.getFullYear() == y && date.getMonth() == m && date.getDate() == d;
 }
